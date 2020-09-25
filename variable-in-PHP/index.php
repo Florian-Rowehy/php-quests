8a3ce23382448fbb17b83a6ce2043cbada2d24e1
Florian-Rowehy <florian.rowehy@gmail.com>
2020-09-25 21:12:50 +0200

    added solution php variable quest

---
+++ variable-in-PHP/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+$title =  'Indiana Jones and the Last Crusade';
+$hasSeenMovie = true;
+$releaseYear = 1989;
+$noteImdb = 8.2;
+
+echo "<p> <pre> 
+titre: $title
+boolean value: $hasSeenMovie  (converted to a number by php)
+release year: $releaseYear
+IMDB note: $noteImdb
+</pre> </p>"
+
+?>
