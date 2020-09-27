d3d17c7003628c7e91b36edb59f9b3ecdeff187f
Florian-Rowehy <florian.rowehy@gmail.com>
2020-09-27 18:50:44 +0200

    added return types of Car & Bicycle methods, comments describing expected types of instance var

--- POO/basics-part1/Bicycle.php
+++ POO/basics-part1/Bicycle.php
@@ -1,13 +1,28 @@
 <?php
 class Bicycle
 {
+    /**
+     * @var int
+     */
     private $nbWheels;
+    /**
+     * @var int
+     */
     private $currentSpeed;
+    /**
+     * @var string
+     */
     private $color;
+    /**
+     * @var int
+     */
     private $nbSeats;
+    /**
+     * @var bool
+     */
     private $hasLuggageRack;
 
-    public function __construct($color, $hasLuggageRack=false)
+    public function __construct(string $color, bool $hasLuggageRack=false)
     {
         $this->nbWheels = 2;
         $this->currentSpeed = 0;
@@ -16,11 +31,11 @@ class Bicycle
         $this->hasLuggageRack = $hasLuggageRack;
     }
 
-    public function forward($speed){
+    public function forward($speed): void{
         $this->currentSpeed += $speed;
     }
 
-    public function brake(){
+    public function brake(): void{
         $reducedSpeed = $this->currentSpeed - 10;
         $this->currentSpeed = $reducedSpeed <0? 0 : $reducedSpeed;
     }
--- POO/basics-part1/Car.php
+++ POO/basics-part1/Car.php
@@ -1,11 +1,29 @@
 <?php 
 
 class Car {
+    /**
+     * @var int
+     */
     private $nbWheels;
+    /**
+     * @var int
+     */
     private $currentSpeed;
+    /**
+     * @var string
+     */
     private $color;
+    /**
+     * @var int
+     */
     private $nbSeats;
+    /**
+     * @var string
+     */
     private $energy;
+    /**
+     * @var int
+     */
     private $energyLevel;
 
     public function __construct(string $color, int $nbSeats, string $energy){
@@ -17,32 +35,32 @@ class Car {
         $this->energyLevel = 100;
     }
 
-    public function forward($speed){
+    public function forward(int $speed) :void {
         $this->currentSpeed += $speed;
     }
-    public function brake(){
+    public function brake(): void {
         $reducedSpeed = $this->currentSpeed - 20;
         $this->currentSpeed = $reducedSpeed <0? 0 : $reducedSpeed;
     }
-    public function start(){
+    public function start(): void{
         $this->currentSpeed = $this->currentSpeed === 0? 5 : $this->currentSpeed;
     }
-    public function getNbWheels(){
+    public function getNbWheels(): int {
         return $this->nbWheels;
     }
-    public function getCurrentSpeed(){
+    public function getCurrentSpeed(): int {
         return $this->currentSpeed;
     }
-    public function getColor(){
+    public function getColor(): string{
         return $this->color;
     }
-    public function getNbSeats(){
+    public function getNbSeats(): int{
         return $this->nbSeats;
     }
-    public function getEnergy(){
+    public function getEnergy(): string{
         return $this->energy;
     }
-    public function getEnergyLevel(){
+    public function getEnergyLevel(): int{
         return $this->energyLevel;
     }
 
